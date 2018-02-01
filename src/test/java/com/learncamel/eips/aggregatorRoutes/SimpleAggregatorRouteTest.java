d59769c7228de45b1af836e12fd943016e5ab2bb
bala-murthy <balamurugan.murthy@gmail.com>
2018-02-01 06:20:22 +0530

    Changes made to test if multiple bodies are recieved and can be tested with OR condition

--- src/test/java/com/learncamel/eips/aggregatorRoutes/SimpleAggregatorRouteTest.java
+++ src/test/java/com/learncamel/eips/aggregatorRoutes/SimpleAggregatorRouteTest.java
@@ -5,6 +5,10 @@ import org.apache.camel.component.mock.MockEndpoint;
 import org.apache.camel.test.junit4.CamelTestSupport;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class SimpleAggregatorRouteTest extends CamelTestSupport {
 
     @Override
@@ -12,16 +16,26 @@ public class SimpleAggregatorRouteTest extends CamelTestSupport {
         return new SimpleAggregatorRoute();
     }
 
-   @Test
+    //List<String> receivedBodies = Arrays.asList("123","321");
+
+
+
+
+
+    @Test
     public void testSimpleAggregatorRoute() throws InterruptedException {
 
        MockEndpoint mockOP = getMockEndpoint("mock:simpleAggregatorMockOP");
-       mockOP.expectedBodiesReceived("123");
 
 
-       template.sendBodyAndHeader("direct:simpleAggregatorIP","3","aggregatorId",1);
-       template.sendBodyAndHeader("direct:simpleAggregatorIP","2","aggregatorId",1);
+        //mockOP.expectedBodiesReceivedInAnyOrder("123","321");
+        //mockOP.expectedMessagesMatches(body().contains("123"),body().contains("321"));
+
+        mockOP.expectedBodiesReceived("321");
+
        template.sendBodyAndHeader("direct:simpleAggregatorIP","1","aggregatorId",1);
+       template.sendBodyAndHeader("direct:simpleAggregatorIP","2","aggregatorId",1);
+       template.sendBodyAndHeader("direct:simpleAggregatorIP","3","aggregatorId",1);
 
        assertMockEndpointsSatisfied();
 
